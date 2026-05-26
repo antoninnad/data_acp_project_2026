9939e759637488a4a9165a85ba720a3b1a8498ff
Justine <justine.idrac@gmail.com>
2026-05-26 17:07:26 +0200

    creation meanFace

--- maths/src/abstraction/PCA.java
+++ maths/src/abstraction/PCA.java
@@ -1,22 +1,23 @@
 package abstraction;
 
 import java.util.List;
-
 import math.Vector;
+import math.Matrix;
+import abstraction.Image;
 
 public class PCA {
 	private int numberOfKeptAxes; // number of axes that are relevant for the ACP
 	private Vector meanFace; //mean face found based on our database
 	
-	
 	/**
 	 * Calculate the mean face based on a list of Images, by averaging pixels by pixels
+	 * 
 	 * @param listImages list of images that has been treated before(resized and greyscale)
 	 * @return vector representing the mean face
 	 * */
-	private Vector getMeanFace(List<Image> listImages) {
+	public Vector getMeanFace(List<Image> listImages) {
 		double mean; // mean of pixels 
-		for(int j = 0; j < ???? ; j++) { //j : index for pixels
+		for(int j = 0; j < Image.getDimension() ; j++) { //j : index for pixels
 			for(int i = 0; i < listImages.size(); i++) { // i : index for images 
 				/*Sum pixels*/
 				mean+=listImages.get(i).getPixel().get(j);
@@ -28,8 +29,7 @@ public class PCA {
 		return meanFace
 	}
 	
-	
-	
+	centeredImages()
 	
 
 }
