68ac4ada6e22a6b51273a300e9ef2f5ffe9ad511
Antonin Nadaud <nadaudantonin@gmail.com>
2026-05-27 18:49:41 +0200

    correcting a mis commit

--- maths/src/abstraction/PCA.java
+++ maths/src/abstraction/PCA.java
@@ -64,8 +64,6 @@ public class PCA {
 			}
 
 			//normalyse by the number of image
-			mean = mean.multiplicationScalar((double) 1.0/numberImg);
-
 			meanFace = mean;
 			return mean;
 
@@ -117,5 +115,97 @@ public class PCA {
 	 * Saves the informations regarding the PCA to avoid recalculating too often
 	 * @param filename is the name of the file where the informations are saved
 	 */
+	public void saveToFile(String filename) {
+		try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
+			//Save the number of axes kept
+			writer.println(this.numberOfKeptAxes);
+			writer.println();
+
+			//Save the mean face
+			writer.println(this.meanFace.getDimension());
+			for (int i = 0 ; i < this.meanFace.getDimension() ; i++) {
+				writer.println(this.meanFace.get(i) + " ");
+
+			}
+			writer.println();
+
+			//Save the coordinates of the faces
+			writer.println();
+			writer.println(this.facesCoordinates.getNbRows() + " " + this.facesCoordinates.getNbColumns());
+			for (int i = 0 ; i < this.facesCoordinates.getNbRows(); i++ ) {
+				for (int j = 0; j < this.facesCoordinates.getNbColumns(); j++) {
+					writer.println(this.facesCoordinates.get(i,j) + " ");
+				}
+				writer.println();
+			}
+
+			writer.println();
+
+			//Save the eigenfaces
+			writer.println(this.eigenfaces.getNbRows() + " " + this.eigenfaces.getNbColumns());
+            for (int i = 0; i < this.eigenfaces.getNbRows(); i++) {
+                for (int j = 0; j < this.eigenfaces.getNbColumns(); j++) {
+                    writer.print(this.eigenfaces.get(i, j) + " ");
+                }
+                writer.println();
+            }
+            writer.println();
+			System.out.println("The informations concerning the PCA are saved to " + filename);
+		} catch (IOException e) {
+			System.err.println("Error while trying to save the informations" + e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Loads the informations regarding the PCA to avoid recalculating too often
+	 * @param filename is the name of the file where the informations are saved
+	 */
+	public void loadFromFile(String filename) {
+		try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
+			if (scanner.hasNextInt()) {
+				//Load numberOfKeptAxes (int)
+                this.numberOfKeptAxes = scanner.nextInt();
+			}
+
+            //Load dimension of vector mean face and the values
+            if (scanner.hasNextInt()) {
+                 int meanFaceDim = scanner.nextInt();
+                 this.meanFace = new Vector(meanFaceDim);
+                 for (int i = 0; i < meanFaceDim; i++) {
+                     this.meanFace.set(i, scanner.nextDouble());
+                 }
+             }
+
+             //Load facesCoordinates matrix
+             if (scanner.hasNextInt()) {
+                 int rowsOfFaces = scanner.nextInt();
+                 int colsOfFaces = scanner.nextInt();
+                 this.facesCoordinates = new Matrix(rowsOfFaces, colsOfFaces);
+                 for (int i = 0; i < rowsOfFaces; i++) {
+                     for (int j = 0; j < colsOfFaces; j++) {
+                         this.facesCoordinates.set(i, j, scanner.nextDouble());
+                     }
+                 }
+             }
+
+             //Load eigenfaces
+             if (scanner.hasNextInt()) {
+                 int rowsOfEigenfaces = scanner.nextInt();
+                 int colsOfEigenfaces = scanner.nextInt();
+                 this.eigenfaces = new Matrix(rowsOfEigenfaces, colsOfEigenfaces);
+                 for (int i = 0; i < rowsOfEigenfaces; i++) {
+                     for (int j = 0; j < colsOfEigenfaces; j++) {
+                         this.eigenfaces.set(i, j, scanner.nextDouble());
+                     }
+                 }
+             }
+
+             System.out.println("The informations concerning the PCA are successfully loaded from " + filename);
+
+		} catch (IOException e) {
+			System.err.println("Error while trying to load the informations" + e.getMessage());
+		}
+	}
 
 }
