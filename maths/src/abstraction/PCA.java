cf9bf8ad88cd3858198886f5fcc6a9fabf199f77
Justine <justine.idrac@gmail.com>
2026-05-27 16:53:12 +0200

    Création centeredImages

--- maths/src/abstraction/PCA.java
+++ maths/src/abstraction/PCA.java
@@ -1,5 +1,7 @@
 package abstraction;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import math.Vector;
 import math.Matrix;
@@ -10,26 +12,48 @@ public class PCA {
 	private Vector meanFace; //mean face found based on our database
 	
 	/**
-	 * Calculate the mean face based on a list of Images, by averaging pixels by pixels
+	 * Calculate the mean face based on a list of images
 	 * 
-	 * @param listImages list of images that has been treated before(resized and greyscale)
+	 * @param list of images that has been treated before(resized and greyscale)
 	 * @return vector representing the mean face
 	 * */
-	public Vector getMeanFace(List<Image> listImages) {
-		double mean; // mean of pixels 
-		for(int j = 0; j < Image.getDimension() ; j++) { //j : index for pixels
-			for(int i = 0; i < listImages.size(); i++) { // i : index for images 
+	private Vector getMeanFace(List<Image> listImages) {
+		double mean;
+		for(int j = 0; j < listImages.get(0).getPixel().getDimension(); j++ ) {
+			for(int i = 0; i< listImages.size(); i++) {
 				/*Sum pixels*/
 				mean+=listImages.get(i).getPixel().get(j);
 			}
-			/*add the mean in the of the pixels in meanFace */
 			meanFace.add(mean/listImages.size());
-			mean = 0;			
+			mean = 0;		
 		}
-		return meanFace
+		return meanFace;
 	}
 	
-	centeredImages()
-	
+
+	/**
+	 * Center images with the mean face
+	 * 
+	 * @param list of images (rezised and greyscale)
+	 * @return matrix of centered images
+	 * */
+	public Matrix centeredImages(List<Image> listImages) {
+		Matrix centeredMatrix = new Matrix(listImages.get(0).getPixel().getDimension(), listImages.size());
+		for(int i=0; i<listImages.size(); i++) {			
+			try {
+				//substracts pixels to get a centered vector and add to centeredList
+				Vector centeredVector = listImages.get(i).getPixel().difference(meanFace);
+				//add in the matrix the new centered image
+				 for (int j = 0; j < centeredVector.getDimension(); j++) {
+					 centeredMatrix.set(j, i, centeredVector.get(j)); 
+				 }
+			} catch (IOException e) {
+				//case if the file was not found
+				e.printStackTrace();
+            }
+		}
+		return centeredMatrix;
+	}
 
 }
+	
