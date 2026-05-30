9ff2ea8e3f5d9d37caf2f8518b22c95cf0ef4d0e
u-1074 <>
2026-05-30 16:25:35 +0200

    Getting of javafx

--- maths/src/module-info.java
+++ maths/src/module-info.java
@@ -6,9 +6,5 @@
  */
 module maths {
     requires java.desktop;
-    requires java.base;
-    requires javafx.base;
-    requires javafx.graphics;
-    requires javafx.controls;
-    requires javafx.fxml;
+
 }
